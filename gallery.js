682add130e64567940fcea876fcdf544cf64740a
Stefan <milovicstefan01@gmail.com>
2024-08-30 12:52:13 +0200

    Creating Our Works Page

--- gallery.js
+++ gallery.js
@@ -8,3 +8,21 @@ gallery : {
 }
 });
 });
+$(document).ready(function() {
+    var popup_btn = $('.popup-btn-two');
+    popup_btn.magnificPopup({
+    type : 'image',
+    gallery : {
+        enabled : true
+    }
+    });
+    });
+    $(document).ready(function() {
+        var popup_btn = $('.popup-btn-three');
+        popup_btn.magnificPopup({
+        type : 'image',
+        gallery : {
+            enabled : true
+        }
+        });
+        });
